0403d6a7efedec3a60eeb9938304da156f97f37d
bryandarioui <bryan.dario@ui.ac.id>
2021-05-27 18:12:25 +0700

    Modul 9 - Case Study

---
+++ app/src/main/java/bryandariolesmana/jwork_android/Location.java
@@ -0,0 +1,42 @@
+package bryandariolesmana.jwork_android;
+
+public class Location {
+    private String province;
+    private String description;
+    private String city;
+
+    public Location(String province, String city,String description ){
+        this.province = province;
+        this.city = city;
+        this.description = description;
+    }
+    public String getProvince(){
+        return this.province;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setProvince(String province){
+        this.province = province;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String toString(){
+        return "Province = " + province +
+                "\nCity = " + city +
+                "\nDescription = " + description;
+    }
+}
--- app/src/main/java/bryandariolesmana/jwork_android/LoginActivity.java
+++ app/src/main/java/bryandariolesmana/jwork_android/LoginActivity.java
@@ -2,6 +2,7 @@ package bryandariolesmana.jwork_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,31 +10,62 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
         EditText editTextEmail = findViewById(R.id.editTextEmail);
         EditText editTextPassword = findViewById(R.id.editTextPassword);
         Button buttonLogin = findViewById(R.id.ButtonText);
         TextView plainTextRegister = findViewById(R.id.ViewText);
 
-        Object btnLogin;
-        btnLogin.setOnCLickListener(new View.OnClickListener(){
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view){
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
-                if(email.equals("test@test.com") && password.equals("test")){
-                    Toast.makeText(LoginActivity.this, "Login Successful",
-                            Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(LoginActivity.this, "Login Failed", 
-                            Toast.LENGTH_SHORT).show();}
-                }
-            });
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if ( jsonObject != null ) {
+                                Toast.makeText(LoginActivity.this, "Login Successful",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this,
+                                        MainActivity.class);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException error){
+                            Toast.makeText(LoginActivity.this, "Login Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        plainTextRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this,
+                        RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
